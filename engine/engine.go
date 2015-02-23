f55c594b351c5a519e120b31612d1bdb42117e4b
Didip Kerabat <didip@newrelic.com>
2015-02-22 16:41:34 -0800

    Able to generate index.md and index.html

--- engine/engine.go
+++ engine/engine.go
@@ -21,13 +21,15 @@ func NewBlog(title string) *Engine {
 	e := &Engine{}
 	e.Title = title
 	e.CurrentDir = "$GOPATH/src/github.com/didip/didip.github.io"
+	e.MaxSummaryParagraphs = 3
 
 	return e
 }
 
 type Engine struct {
-	Title      string
-	CurrentDir string
+	Title                string
+	CurrentDir           string
+	MaxSummaryParagraphs int
 }
 
 func (e *Engine) ServerPort() string {
@@ -98,11 +100,127 @@ func (e *Engine) GenerateAllPostsHTML() error {
 		"posts")
 
 	manyFileInfo, err := ioutil.ReadDir(mdDir)
+	if err != nil {
+		return err
+	}
 
 	for _, fileinfo := range manyFileInfo {
-		println(fileinfo.Name())
 		e.GeneratePostHTML(filepath.Join(mdDir, fileinfo.Name()))
 	}
+
+	return err
+}
+
+func (e *Engine) GenerateAllHTML() error {
+	err := e.GenerateAllPostsHTML()
+	if err != nil {
+		return err
+	}
+
+	return e.GenerateIndexHTML()
+}
+
+func (e *Engine) MarkdownSummary(inpath string) (map[string]string, error) {
+	data := make(map[string]string)
+
+	markdownData, err := ioutil.ReadFile(libstring.ExpandTildeAndEnv(inpath))
+	if err != nil {
+		return data, err
+	}
+
+	text := make([]string, 0)
+	newParagraphCounter := 0
+
+	for i, line := range strings.Split(string(markdownData), "\n") {
+		if i == 0 {
+			data["Title"] = strings.Replace(line, "## ", "", 1)
+			continue
+		}
+
+		text = append(text, line)
+
+		if strings.TrimSpace(line) == "" {
+			println(line)
+			newParagraphCounter++
+		}
+		if newParagraphCounter > e.MaxSummaryParagraphs {
+			break
+		}
+	}
+
+	if len(text) > 0 {
+		data["Text"] = strings.Join(text, "\n")
+	}
+
+	return data, err
+}
+
+func (e *Engine) GenerateIndexMarkdown() error {
+	postsMarkdownDir := filepath.Join(
+		libstring.ExpandTildeAndEnv(e.CurrentDir),
+		"markdown",
+		"posts")
+
+	manyFileInfo, err := ioutil.ReadDir(postsMarkdownDir)
+	if err != nil {
+		return err
+	}
+
+	content := make([]string, 0)
+
+	for _, fileinfo := range manyFileInfo {
+		postLink := "/posts/" + fileinfo.Name()
+		postLink = strings.Replace(postLink, ".md", ".html", -1)
+
+		summary, err := e.MarkdownSummary(filepath.Join(postsMarkdownDir, fileinfo.Name()))
+		if err != nil {
+			continue
+		}
+
+		// Generate Link
+		content = append(content, fmt.Sprintf("[<h2>%v</h2>](%v)\n", summary["Title"], postLink))
+
+		// Generate Summary
+		if _, ok := summary["Text"]; ok {
+			content = append(content, fmt.Sprintf("%v\n", summary["Text"]))
+		}
+	}
+
+	indexMarkdownFilename := filepath.Join(
+		libstring.ExpandTildeAndEnv(e.CurrentDir),
+		"markdown",
+		"index.md")
+
+	err = ioutil.WriteFile(indexMarkdownFilename, []byte(strings.Join(content, "\n")), 0644)
+
+	return err
+}
+
+func (e *Engine) GenerateIndexHTML() error {
+	err := e.GenerateIndexMarkdown()
+	if err != nil {
+		return err
+	}
+
+	inpath := filepath.Join(
+		libstring.ExpandTildeAndEnv(e.CurrentDir),
+		"markdown",
+		"index.md")
+
+	filename := path.Base(inpath)
+	filename = strings.Replace(filename, ".md", ".html", -1)
+
+	markdownData, err := ioutil.ReadFile(inpath)
+	if err != nil {
+		return err
+	}
+
+	html := blackfriday.MarkdownCommon(markdownData)
+
+	err = ioutil.WriteFile(
+		filepath.Join(
+			libstring.ExpandTildeAndEnv(e.CurrentDir),
+			filename), html, 0644)
 
 	return err
 }
--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 			Action: func(c *cli.Context) {
 				// Watches markdown/posts/ directory and auto-generate HTML
 				go blog.WatchDir(path.Join(blog.CurrentDir, "markdown", "posts"), func(event fsnotify.Event) {
-					blog.GenerateAllPostsHTML()
+					blog.GenerateAllHTML()
 				})
 
 				http.Handle("/", http.FileServer(http.Dir(".")))
@@ -48,7 +48,7 @@ func main() {
 					path := c.Args().Get(1)
 
 					if path == "" {
-						blog.GenerateAllPostsHTML()
+						blog.GenerateAllHTML()
 					} else {
 						blog.GeneratePostHTML(path)
 					}
