c376db69017a67a9a16f54b9207a25ede1fc80ea
Didip Kerabat <didipk@gmail.com>
2015-02-22 23:48:06 -0800

    Better layout.

--- engine/templates/simple.go
+++ engine/templates/simple.go
@@ -17,6 +17,8 @@ func Generate(title string, html []byte, outpath string) error {
 
     <link rel="stylesheet" href="//yui.yahooapis.com/pure/0.5.0/pure-min.css">
     <link rel="stylesheet" href="//yui.yahooapis.com/pure/0.5.0/grids-responsive-min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,500" type="text/css">
     <link rel="stylesheet" href="/css/layouts/blog.css">
 
     <!--[if IE]>
@@ -29,15 +31,15 @@ func Generate(title string, html []byte, outpath string) error {
 <div id="layout" class="pure-g">
     <div class="sidebar pure-u-1 pure-u-md-1-4">
         <div class="header">
-            <h1 class="brand-title">%v</h1>
+            <h1 class="brand-title"><a href="/">%v</a></h1>
 
             <nav class="nav">
                 <ul class="nav-list">
                     <li class="nav-item">
-                        <a href="//twitter.com/didip">Twitter</a>
+                        <a href="//twitter.com/didip"><i class="fa fa-twitter"></i> Twitter</a>
                     </li>
                     <li class="nav-item">
-                        <a href="//github.com/didip">GitHub</a>
+                        <a href="//github.com/didip"><i class="fa fa-github"></i> GitHub</a>
                     </li>
                 </ul>
             </nav>
